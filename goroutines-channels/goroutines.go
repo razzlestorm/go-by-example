698ad08f26c44464892a2920f494ed8a1f4d2681
Jerimiah Willhite <>
2021-10-13 23:09:53 -0700

    add basic goroutines.go

---
+++ goroutines-channels/goroutines.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for ii := 0; ii < 3; ii++ {
+		fmt.Println(from, ":", ii)
+	}
+}
+
+func main() {
+	// Suppose we have a function call f(s). Here’s how we’d call that in the usual way, running it synchronously.
+	f("direct")
+
+	// To invoke this function in a goroutine, use go f(s). This new goroutine will execute concurrently with the calling one.
+	go f("goroutine")
+
+	// You can also start a goroutine for an anonymous function call.
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	// Our two function calls are running asynchronously in separate goroutines now. Wait for them to finish (for a more robust approach, use a WaitGroup).
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	//When we run this program, we see the output of the blocking call first, then the output of the two goroutines. The goroutines’ output may be interleaved, because goroutines are being run concurrently by the Go runtime.
+
+}
