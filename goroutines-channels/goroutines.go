da8b075dbcb4373a05105156a6a64d4ce9476b2c
Jerimiah Willhite <>
2021-10-25 21:13:30 -0700

    add basic channels info to goroutines.go

--- goroutines-channels/goroutines.go
+++ goroutines-channels/goroutines.go
@@ -29,4 +29,19 @@ func main() {
 
 	//When we run this program, we see the output of the blocking call first, then the output of the two goroutines. The goroutines’ output may be interleaved, because goroutines are being run concurrently by the Go runtime.
 
+	// Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+
+	//Create a new channel with make(chan val-type). Channels are typed by the values they convey.
+	messages := make(chan string)
+
+	//Send a value into a channel using the channel <- syntax. Here we send "ping" to the messages channel we made above,
+	// from a new goroutine.
+	go func() { messages <- "ping" }()
+
+	//The <-channel syntax receives a value from the channel. Here we’ll receive the "ping" message we sent above and print it out.
+	msg := <-messages
+	fmt.Println(msg)
+	//When we run the program the "ping" message is successfully passed from one goroutine to another via our channel.
+	// By default sends and receives block until both the sender and receiver are ready. This property allowed us to wait at the end of our program for the "ping" message without having to use any other synchronization.
+
 }
